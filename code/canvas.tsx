77f399303a09a99b9d325721f18a454c96093481
fibo <casati_gianluca@yahoo.it>
2019-12-23 20:28:36 +0100

    landing page scroll

--- code/canvas.tsx
+++ code/canvas.tsx
@@ -1,3 +1,11 @@
 // WARNING: this file is auto generated, any changes will be lost
 import { createDesignComponent, CanvasStore } from "framer"
 const canvas = CanvasStore.shared(); // CANVAS_DATA;
+export const Hero = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_i0dt9kdpM", {}, 1440,810);
+
+export const colors = Object.freeze({
+    /** #0C3D8D */
+    "Blue Gradient 1": "var(--token-d63309bf-c8d7-4343-965e-65e542bd3e6a, rgb(12, 61, 141))",
+    /** #1854A5 */
+    "Blue Gradient 2": "var(--token-ba704c08-fbc3-4c47-b115-534eb1eb509b, rgb(24, 84, 165))",
+})
