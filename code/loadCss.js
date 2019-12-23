9946fdc25e886ad94caa736b83703ebbce8a0a71
fibo <casati_gianluca@yahoo.it>
2019-12-23 19:33:17 +0100

    load css

---
+++ code/loadCss.js
@@ -0,0 +1,11 @@
+if (document.getElementById('bulma') === null) {
+  const bulma = document.createElement('link')
+  css.href = './bulma.css'
+  css.id = 'bulma'
+}
+
+if (document.getElementById('fonts') === null) {
+  const bulma = document.createElement('link')
+  css.href = './fonts.css'
+  css.id = 'fonts'
+}
